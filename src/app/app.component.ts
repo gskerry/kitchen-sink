e1a73aef0f09b87e54bab3f72a5797baf6124ecb
gskerry <gregory.skerry@gmail.com>
2017-02-10 07:18:50 +0000

    bind selected Hero as arg to hero-detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -75,7 +75,7 @@ const HEROES: Hero[] = [
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-        <my-hero-detail></my-hero-detail>
+        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
     `
 })
 
