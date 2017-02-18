04cfe98d9303760a886255840d175d493bbe6afc
gskerry <gregory.skerry@gmail.com>
2017-02-18 23:03:35 +0000

    restore heroService import to HeroesComp - still needed though not registered as provider in HeroesComp

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { Hero } from './hero'; 
+import { HeroService } from './hero.service'
 
 @Component({
     selector: 'my-heroes',
