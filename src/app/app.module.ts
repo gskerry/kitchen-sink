3d2264d1c5783a8b095cc3cf20e4e96946b2b386
gskerry <gregory.skerry@gmail.com>
2017-02-19 17:46:20 +0000

    modulize routing into own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router'
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent }  from './dashboard.component';
@@ -9,29 +8,13 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service'
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
     imports: [ 
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [ 
         AppComponent,
@@ -42,4 +25,5 @@ import { HeroService } from './hero.service'
     providers: [ HeroService ],
     bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {}
