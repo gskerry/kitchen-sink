ab79f85c4a21e2451324265abd2708addcf4386e
gskerry <gregory.skerry@gmail.com>
2017-02-21 01:25:26 +0000

    hero-searchComp imports

--- src/app/hero-search.component.ts
+++ src/app/hero-search.component.ts
@@ -1,6 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
 
+import 'rxjs/add/observable/of'; // Observable class extensions
 
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
+import { HeroSearchService } from './hero-search.service';
+import { Hero } from './hero';
 
 
 @Component({
