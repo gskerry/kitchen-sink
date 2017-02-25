80b54e4121b020ca4ca07d25b7c0ae164d26eeec
gskerry <gregory.skerry@gmail.com>
2017-02-25 15:51:53 +0000

    refactor flatfiledb into API endpoint

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,31 @@
+'use strict'
 var router = require('express').Router();
+const Promise = require("bluebird");
+const path = require('path');
+
+let readFile_prom = Promise.promisify(require("fs").readFile);
+let here = path.join(__dirname, './')
 
 router.get('/', function (req, res) {
-    console.log("hit api.");
-    res.send("hit api")
+    
+    readFile_prom(here+'data.json') //, 'utf-8'
+        .then(function(data){
+            // console.log(typeof data);
+            // let msg = JSON.stringify(data)
+            let content = data.toString('utf-8');
+            // console.log(typeof content);
+            let array = JSON.parse(content)
+            // console.log(typeof array);
+            let query = array.find(function(obj){
+                return obj.id === 1;
+            })
+            // res.json(array)
+            console.log(query);
+            res.json(query)
+            // res.send(msg[0].msg);
+        });
+        // res.send('hello world');
+
 });
 
 module.exports = router;
