3879900872b7f4fb940b472f5371f45bdf204072
gskerry <gregory.skerry@gmail.com>
2017-02-25 22:51:00 +0000

    array.find returning undefined for req.param match... found param is string (vs num in array id)

--- server/api/index.js
+++ server/api/index.js
@@ -21,21 +21,24 @@ router.get('/myheroes/:id', function (req, res) {
     
     readFile_prom(here+'data.json') //, 'utf-8'
         .then(function(data){
+            console.log(req.params.id);
+            console.log(typeof req.params.id);
             // console.log(typeof data);
             // let msg = JSON.stringify(data)
             let content = data.toString('utf-8');
             // console.log(typeof content);
             let array = JSON.parse(content)
+            console.log(array);
             // console.log(typeof array);
             let query = array.find(function(obj){
-                return obj.id === 1;
+                console.log(typeof obj.id);
+                return obj.id === req.params.id;
             })
-            // res.json(array)
-            // console.log(JSON.stringify(query)+' | '+typeof query);
-            // res.json(query)
+            console.log(query);
+            console.log(JSON.stringify(query)+' | '+typeof query);
+            res.json(query)
             // res.send(msg[0].msg);
-            console.log(req.params.id);
-            res.send('hello world');
+            // res.send('hello world');
         });
 
 });
