2f484495ff0bdff5b324d95ead20f1f1c62f24ba
gskerry <gregory.skerry@gmail.com>
2017-02-26 04:17:15 +0000

    additional cleaning

--- server/api/index.js
+++ server/api/index.js
@@ -8,45 +8,30 @@ let readFile_prom = Promise.promisify(require("fs").readFile);
 let here = path.join(__dirname, './')
 
 router.get('/myheroes', function (req, res) {
-    
     readFile_prom(here+'data.json') //, 'utf-8'
         .then(function(data){
             let content = data.toString('utf-8');
             let array = JSON.parse(content)
             res.json(array)
         });
-
 });
 
 router.get('/myheroes/:id', function (req, res) {
-    
     readFile_prom(here+'data.json') //, 'utf-8'
         .then(function(data){
             console.log(req.params.id);
-            console.log(typeof req.params.id);
             let numid = Number(req.params.id)
-            // console.log(typeof data);
-            // let msg = JSON.stringify(data)
             let content = data.toString('utf-8');
-            // console.log(typeof content);
             let array = JSON.parse(content)
-            console.log(array);
-            // console.log(typeof array);
             let query = array.find(function(obj){
-                console.log(typeof obj.id);
                 return obj.id === numid;
             })
             console.log(query);
-            console.log(JSON.stringify(query)+' | '+typeof query);
             res.json(query)
-            // res.send(msg[0].msg);
-            // res.send('hello world');
         });
-
 });
 
 router.post('/myheroes', function (req, res) {
-    
     readFile_prom(here+'data.json')
         .then(function(data){
             let newobj = {} 
@@ -73,17 +58,7 @@ router.post('/myheroes', function (req, res) {
             } else {
                 console.log('that id already exists');
             }
-            
-            // let query = array.find(function(obj){
-            //     console.log(typeof obj.id);
-            //     return obj.id === numid;
-            // })
-            // console.log(query);
-            // console.log(JSON.stringify(query)+' | '+typeof query);
-            // res.send(msg[0].msg);
-            // res.send('hello world');
         });
-
 });
 
 module.exports = router;
