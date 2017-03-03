859300861852378669dd959b928a0b7a265abd30
gskerry <gregory.skerry@gmail.com>
2017-03-03 17:27:25 +0000

    refactor hero service to generic endpoint variable. Swap as needed.

--- src/ts/hero.service.ts
+++ src/ts/hero.service.ts
@@ -8,21 +8,21 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
     
-    private heroesUrl = 'api/heroes'; // (!) hardcoded to Angular's web-api 
-    private myheroesUrl = 'api/myheroes'; // refactor for express API 
+    // private endpoint = 'api/heroes'; // (!) hardcoded to Angular's web-api 
+    private endpoint = 'api/heroes/flatfile'; // refactor for express API 
     private headers = new Headers({'Content-Type':'application/json'})
 
     constructor(private http: Http){}
 
     getHeroes(): Promise<Hero[]> {
-        return this.http.get(this.myheroesUrl)
+        return this.http.get(this.endpoint)
             .toPromise() // execute rxjs promise-converter
             .then(response => response.json() as Hero[]) // native http resp
             .catch((err) => {this.handleError(err, 'getHeroes')});
     }
     
     getHero(id: number): Promise<Hero> {
-        const url =`${this.myheroesUrl}/${id}`;
+        const url =`${this.endpoint}/${id}`;
         return this.http.get(url)
             .toPromise()
             .then(response => response.json() as Hero)
@@ -30,7 +30,7 @@ export class HeroService {
     }
     
     update(hero: Hero): Promise<Hero> {
-        const url =`${this.myheroesUrl}/${hero.id}`;
+        const url =`${this.endpoint}/${hero.id}`;
         return this.http
             .put(url, JSON.stringify(hero), {headers: this.headers})
             .toPromise()
@@ -40,14 +40,14 @@ export class HeroService {
 
     create(name: string): Promise<Hero>{
         return this.http
-            .post(this.myheroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .post(this.endpoint, JSON.stringify({name: name}), {headers: this.headers})
             .toPromise()
             .then(res => res.json())
             .catch((err) => {this.handleError(err, 'createhero')});
     }
     
     delete(id: number): Promise<void>{
-        const url =`${this.myheroesUrl}/${id}`;
+        const url =`${this.endpoint}/${id}`;
         return this.http
             .delete(url, {headers: this.headers})
             .toPromise()
